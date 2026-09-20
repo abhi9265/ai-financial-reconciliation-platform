cf5ea80d3d6428d768586a5049f0e3e9cc1166c4
Abhishek Singh <153421645+abhi9265@users.noreply.github.com>
2026-09-20 13:24:40 +0530

    feat: complete reconciliation MVP foundation

---
+++ src/reconciliation_platform/reconciliation/__init__.py
@@ -0,0 +1,3 @@
+"""Reconciliation package."""
+from reconciliation_platform.reconciliation.engine import MatchCandidate, ReconciliationDecision, reconcile
+__all__ = ["MatchCandidate", "ReconciliationDecision", "reconcile"]
