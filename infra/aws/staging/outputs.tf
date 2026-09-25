f90f79b0e3a4fa20e2bcfdea0e58519ff05f6676
Abhishek Singh <153421645+abhi9265@users.noreply.github.com>
2026-09-25 13:36:45 +0530

    docs: add staging Terraform outputs and examples

---
+++ infra/aws/staging/outputs.tf
@@ -0,0 +1,23 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+output "staging_url" {
+  value = "https://${var.staging_domain}"
+}
+output "alb_dns_name" {
+  value = aws_lb.staging.dns_name
+}
+output "database_endpoint" {
+  value = aws_db_instance.staging.address
+  sensitive = true
+}
+output "redis_endpoint" {
+  value = aws_elasticache_replication_group.staging.primary_endpoint_address
+  sensitive = true
+}
+output "s3_bucket" {
+  value = aws_s3_bucket.objects.bucket
+}
+output "api_secret_name" {
+  value = aws_secretsmanager_secret.api_key.name
+}
